9dd01ce74cde31a626fd03766fcbbbaf5d9cf35c
Alex Ligkas <ligkalex@gmail.com>
2020-11-27 02:15:24 +0200

    move hello fn and its test to a greetings package

---
+++ greetings/hello.go
@@ -0,0 +1,36 @@
+package greetings
+
+// Supported languages
+const (
+	greek  = "Greek"
+	french = "French"
+)
+
+// Language prefixes
+const (
+	greekPrefix        = "Geia sou "
+	englishHelloPrefix = "Hello, "
+	frenchPrefix       = "Bonjour, "
+)
+
+const defaultName = "World"
+
+func Hello(name, language string) string {
+
+	if name == "" {
+		return greetingPrefix(language) + defaultName
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) string {
+	switch language {
+	case greek:
+		return greekPrefix
+	case french:
+		return frenchPrefix
+	default:
+		return englishHelloPrefix
+	}
+}
--- main.go
+++ main.go
@@ -2,41 +2,6 @@ package main
 
 import "fmt"
 
-// Supported languages
-const (
-	greek  = "Greek"
-	french = "French"
-)
-
-// Language prefixes
-const (
-	greekPrefix        = "Geia sou "
-	englishHelloPrefix = "Hello, "
-	frenchPrefix       = "Bonjour, "
-)
-
-const defaultName = "World"
-
-func Hello(name, language string) string {
-
-	if name == "" {
-		return greetingPrefix(language) + defaultName
-	}
-
-	return greetingPrefix(language) + name
-}
-
-func greetingPrefix(language string) string {
-	switch language {
-	case greek:
-		return greekPrefix
-	case french:
-		return frenchPrefix
-	default:
-		return englishHelloPrefix
-	}
-}
-
 func main() {
 
 	fmt.Println(Hello("World", "Greek"))
