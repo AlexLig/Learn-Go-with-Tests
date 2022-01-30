e65aaad495f445a32c917d5d7632da712c16c280
Alex Ligkas <a.ligkas@ferryhopper.com>
2022-01-30 20:50:26 +0200

    change: test case to any size of number collection

--- integers/sum_test.go
+++ integers/sum_test.go
@@ -3,17 +3,13 @@ package integers
 import "testing"
 
 func TestSum(t *testing.T) {
-
-	t.Run("number collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-
+	t.Run("number collection of any size", func(t *testing.T) {
+		numbers := []int{3, 2, 1}
 		got := Sum(numbers)
-		want := 15
-
+		want := 6
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
-
 	})
 
 }
